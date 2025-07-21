977b8384d9adb821600eaf06cd1164d3b8bdafa8
David M <207745043+davidmillen50@users.noreply.github.com>
2025-07-21 17:11:41 +0100

    feat: set up bauhaus style navbar and sidebar

--- src/components/Lessons.tsx
+++ src/components/Lessons.tsx
@@ -14,15 +14,22 @@ const data = await fetch(import.meta.env.VITE_API_BASE_URL + '/lessons', {
 
 const Lessons: React.FC = () => {
   return (
-    <article>
-      <h1>My First Component</h1>
-      <ol>
-        <li>Components: UI Building Blocks</li>
-        <li>Defining a Component</li>
-        <li>Using a Component</li>
-      </ol>
-      <div>{JSON.stringify(data)}</div>
-    </article>
+    <div>
+    <nav className="top-navbar" />
+      <div className="bauhaus-art-sidebar">
+        <div className="shape black-circle"></div>
+        <div className="shape yellow-rect"></div>
+        <div className="shape blue-line"></div>
+        <div className="shape orange-circle"></div>
+        <div className="shape yellow-circle"></div>
+        <div className="shape black-line"></div>
+        <div className="shape black-rect"></div>
+        <div className="shape md-black-square"></div>
+        <div className="shape lower-black-line"></div>
+        <div className="shape horizontal-black-line"></div>
+        <div className="shape red-rect"></div>
+      </div>
+    </div>
   )
 }
 
